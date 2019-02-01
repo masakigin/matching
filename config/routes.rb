00114847073fd3b74b2d345e5180ffaa7b245c71
山本真輝 <yamamotomasaki@MYnoMacBook-puro.local>
2019-02-01 23:46:59 +0900

    chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i(index show)
   resources :reactions, only: %i(create)
+  resources :matching, only: %i(index)
+  resources :chat, only: %i(create show)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
